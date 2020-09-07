13f7a47fa1f21e2e45ba57476e2028eaa55d217c
Aleksandr Zharkevich <alexandr.zharkevich@gmail.com>
2020-09-07 21:57:09 +0300

    JDBC security

---
+++ src/main/java/by/mrbregovich/tacos/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package by.mrbregovich.tacos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username, password, enabled from Users where username=?"
+                )
+                .authoritiesByUsernameQuery(
+                        "select username, authority from UserAuthorities where username=?"
+                )
+                .passwordEncoder(new SCryptPasswordEncoder());
+    }
+}
